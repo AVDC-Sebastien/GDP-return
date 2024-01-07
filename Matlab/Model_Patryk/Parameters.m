603ca3fa0c04e1f6afd5f0c4bc4728879c4762bb
Paxon57 <106776707+paxon57@users.noreply.github.com>
2024-01-07 16:55:00 +0000

    Probably enough done to show on Monday

---
+++ Matlab/Model_Patryk/Parameters.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% VEHICLE PARAMETERS %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+CT_Z = 0.2; % Distance of Centre of Thrust from the ground
+CT_G = 0.8; % Distance of Centre of Gravity from the ground
+
+Mass = 2; % Mass in kg
