c80987182a96b7c076386990c5c04daee73fbd54
Paxon57 <106776707+paxon57@users.noreply.github.com>
2024-01-09 20:15:42 +0000

    Overly Basic Aerodynamic Drag

--- Matlab/Model_Patryk/Parameters.m
+++ Matlab/Model_Patryk/Parameters.m
@@ -6,3 +6,7 @@ CT_Z = 0.2; % Distance of Centre of Thrust from the ground
 CT_G = 0.8; % Distance of Centre of Gravity from the ground
 
 Mass = 2; % Mass in kg
+
+% Aerodynamics
+C_d = 1.05;     % Drag coefficient
+Area = 0.1;   % Area
