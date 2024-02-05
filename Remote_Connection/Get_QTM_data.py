3ea27a968106fb0b449a38d78e59264f9c92881c
SébastienL <155736383+AVDC-Sebastien@users.noreply.github.com>
2024-02-05 14:07:11 +0000

    Update Get_QTM_data.py

---
+++ Remote_Connection/Get_QTM_data.py
@@ -0,0 +1,25 @@
+# pip install qtm
+import asyncio
+import qtm_rt
+
+def on_packet(packet):
+    """ Callback function that is called everytime a data packet arrives from QTM """
+    print("Framenumber: {}".format(packet.framenumber))
+    header, markers = packet.get_3d_markers()
+    print("Component info: {}".format(header))
+    for marker in markers:
+        print("\t", marker)
+
+
+async def setup():
+    """ Main function """
+    connection = await qtm_rt.connect("127.0.0.1")
+    if connection is None:
+        return
+
+    await connection.stream_frames(components=["3d"], on_packet=on_packet)
+
+
+if __name__ == "__main__":
+    asyncio.ensure_future(setup())
+    asyncio.get_event_loop().run_forever()
