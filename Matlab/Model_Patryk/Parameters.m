08c50a363d2cb7836eb46a5be92d8ef152ea0638
Paxon57 <106776707+paxon57@users.noreply.github.com>
2024-02-06 21:01:04 +0000

    A bit of everything

--- Matlab/Model_Patryk/Parameters.m
+++ Matlab/Model_Patryk/Parameters.m
@@ -4,11 +4,19 @@
 
 %%% REFERENCE POINT - Basically ground level when rocket is idle on the ground
 
-CT_Z = 0.2; % Vertical Distance of Centre of Thrust from the reference point
-CT_G = 0.8; % Vertical Distance of Centre of Gravity from the reference point
+ct_pos = [0 0 0.2]; % Vertical Distance of Centre of Thrust from the reference point
+cg_pos = [0.03 0 0.7]; % Vertical Distance of Centre of Gravity from the reference point
 
 Mass = 2; % Mass in kg
 
 %%% Aerodynamics %%%
-C_d = 1.05;     % Drag coefficient
+C_d = 1.05;   % Drag coefficient
 Area = 0.1;   % Area
+
+%%% Sensor Placement %%%
+%dist_pos = % Distance Sensor Position
+
+%%% Kalman Filter Setup %%%
+load("Kalman_SS");
+Q = eye(16)*0.02;
+R = eye(6)*0.05;
