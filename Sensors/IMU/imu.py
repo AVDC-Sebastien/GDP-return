62106081d1e17c88df25bf115b47cf1864b1eba3
SébastienL <155736383+AVDC-Sebastien@users.noreply.github.com>
2024-02-22 09:29:44 +0000

    IMU-data

--- Sensors/IMU/imu.py
+++ Sensors/IMU/imu.py
@@ -1,4 +1,3 @@
-import time
 import board
 import adafruit_bno055
 
@@ -7,13 +6,6 @@ i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 while True:
-    print("Temperature: {} degrees C".format(sensor.temperature))
     print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
-    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
     print("Gyroscope (rad/sec): {}".format(sensor.gyro))
-    print("Euler angle: {}".format(sensor.euler))
-    print("Quaternion: {}".format(sensor.quaternion))
-    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
-    print("Gravity (m/s^2): {}".format(sensor.gravity))
     print()
-    time.sleep(1)
