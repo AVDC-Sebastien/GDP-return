00538fb07177d07e5ecdafd0bf797186047b2de9
Paxon57 <106776707+paxon57@users.noreply.github.com>
2024-03-18 11:24:18 +0000

    ...

--- Matlab_Simulation/Model_Patryk/plot_results.m
+++ Matlab_Simulation/Model_Patryk/plot_results.m
@@ -1,10 +1,10 @@
-data = sin01to2;
+data = sin_data;
 
-bias_thrust = 0.031225;
-bias_torque = -0.056332;
+bias_thrust = -0.0113;
+bias_torque = 0.0018;
 
-%thrust_scale = 1;
-thrust_scale = 1/(0.57/2.28);
+thrust_scale = 1;
+%thrust_scale = 1/(0.57/2.28);
 
 % Remove bias
 data(:, 3) = data(:, 3) - bias_torque;
@@ -18,3 +18,6 @@ data(:, 2) = data(:, 2)./1000;
 data(:, 4) = data(:, 4).*thrust_scale;
 
 plot(data, "Time_s_", ["ESCSignal__s_", "Thrust_kgf_", "Torque_N_m_"]);
+
+throttle_data = data(:, 2).ESCSignal__s_;
+thrust_data = data(:, 4).Thrust_kgf_;
