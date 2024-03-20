5f063a7a23c37632c82953c47be8bae71684a8e1
SébastienL <155736383+AVDC-Sebastien@users.noreply.github.com>
2024-03-20 00:39:29 +0000

    Lidar

--- Rocket_Software/Sensors/Lidar/Lidar.py
+++ Rocket_Software/Sensors/Lidar/Lidar.py
@@ -24,14 +24,22 @@ for x in range(100):    # On prend la mesure "repet" fois
    GPIO.output(Trig, True)
    time.sleep(0.00001)
    GPIO.output(Trig, False)
-
+   myStartingTime =time.time()
    while GPIO.input(Echo)==0:  ## Emission de l'ultrason
-     debutImpulsion = time.time()
+    calculation_starting_time = time.time()
+    if calculation_starting_time - myStartingTime > 1:
+        break
+    start = time.time()
+
 
    while GPIO.input(Echo)==1:   ## Retour de l'Echo
-     finImpulsion = time.time()
+    calculation_starting_time = time.time()
+    if calculation_starting_time - myStartingTime > 1:
+        break
+    
+    end = time.time()
 
-   distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
+   distance = round((end - start) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
 
    print ("La distance est de : ",distance," cm, mesure:",x)
 
